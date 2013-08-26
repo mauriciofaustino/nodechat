5b68b4198caa7db71b487dd4e79835fb9dbaea42
Mauricio Faustino <mauricio.faustino@gmail.com>
2013-08-26 07:39:17 -0700

    adicionado dependencia do socket.io

--- app.js
+++ app.js
@@ -30,6 +30,60 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+io.enable("browser client minification");
+io.enable("browser client etag");
+//io.enable("browser client gzip");
+io.set("log level",1);
+io.set("transports", ["xhr-polling"]);
+io.set("polling duration", 10);
+
+io.on("connection", function(client) {
+	console.log("novo cliente conectado");
+	
+	client.on("novo-produto", function(produto) {
+		console.log("recebi um novo produto");
+		client.get("usuario", function(error,usuario) {
+			produto.email=usuario.email;
+			console.log(produto);
+			//client.broadcast.emit("novo-produto-disponivel", produto);
+			io.sockets.in(usuario.categoria).emit("novo-produto-disponivel", produto);
+		});
+		
+	});
+	client.on("disconnect", function() {
+		console.log("desconectando");
+		client.get("usuario", function(error,usuario){
+			var logado = (usuario !== undefined && usuario !== null);
+			if(logado) {
+				console.log("desconectando o usuario " + usuario.email);
+				client.broadcast.emit("logout", usuario.email);
+			}
+		});
+	});
+
+	client.on("login", function(usuario, callback) {
+		console.log(usuario);
+		if(usuario.senha.length >= 6) {
+			client.set("usuario", usuario);
+			client.join(usuario.categoria);
+			callback({
+				sucesso:true,
+				mensagem : "Logado como " + usuario.email + " na categoria " + usuario.categoria
+			})
+		} else {
+			callback({
+				sucesso:false,
+				mensagem : "senha muito curta"
+			})
+		}
+	});
+
+});
+
+
